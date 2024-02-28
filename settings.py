083274bbfc1e643008f6293a0bc6f2526768f1dc
Jamescharge <160158950+Jamescharge@users.noreply.github.com>
2024-02-28 12:21:42 -0800

    Picking up stuff does stuff
    Speed is now the power up item and I coins now work and power ups work

--- settings.py
+++ settings.py
@@ -7,10 +7,10 @@ FPS = 30
 
 TITLE = "SIllY old GamE"
 
-PLAYER_SPEED = 300
+
 
 TILESIZE = 32
-
+PLAYER_SPEED = 300
 
 #silly colors 
 GREEN = (0,255,0)
@@ -18,4 +18,4 @@ BLUE = (0,0,254)
 LIGHTGREY = (75,75,75)
 BGCOLOR = (0,0,0)
 YELLOW = (50,100,230)
-WHITE = (0,0,0)
+WHITE = (255,255,255)
